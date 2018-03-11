8314842c0973f48cf1d1c9fa11f25db64222809d
AdamDWalker <adamwalker03@googlemail.com>
2018-03-11 12:15:52 +0000

    Created python file

---
+++ bibtex_cleaner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+'''
+    File Name: bibtex_cleaner.py
+    Author: Adam Walker
+    Date Created: 11/03/2018
+    Date Last Modified: 11/03/2018
+    Python Version: 3.6.3
+'''
